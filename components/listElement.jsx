778c2cd4fc4c3ea5586d9fc6d1c7bc973b419aba
sho0126hiro <sho0126hiro@gmail.com>
2019-06-27 17:00:12 +0900

    最新

--- components/listElement.jsx
+++ components/listElement.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ListInput from './listInput'
 export default (props) => {
-    // if(true){
     if(props.task.id === props.editingTaskID) {
         return (
             <div>
